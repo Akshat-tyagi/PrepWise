01c5bee7792e04a26b37b1955d77e0a50c5f61a0
Akshat-tyagi <akshattyagi146@gmail.com>
2026-05-25 20:27:52 +0530

    port fix for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,8 @@ app.use("/api/resume", resumeRoutes);
 app.use("/api/interview", interviewRoutes);
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
